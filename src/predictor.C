9e83da7e77879ca187e9b09132fff9fc77547e86
AkankshaMaurya <amaurya@eng.ucsd.edu>
2016-07-13 23:28:56 -0700

    branch predictor

---
+++ src/predictor.C
@@ -0,0 +1,14 @@
+// A very stupid predictor.  It will always predict not taken.
+
+void init_predictor ()
+{
+}
+
+bool make_prediction (unsigned int pc)
+{
+  return false;
+}
+
+void train_predictor (unsigned int pc, bool outcome)
+{
+}
